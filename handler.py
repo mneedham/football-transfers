87a6d8bd0efb4ca6222af4f56941e353abdfe4a9
Mark Needham <m.h.needham@gmail.com>
2019-07-28 07:30:17 +0100

    cli

--- handler.py
+++ handler.py
@@ -10,6 +10,8 @@ import glob
 
 import os.path
 import json
+import click
+from datetime import date
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
@@ -79,8 +81,8 @@ def find_all_pages(event, context):
 
         print("event: {event}".format(event=event))
 
-        start_date = date(2019, 6, 1)
-        end_date = date(2019, 7, 24)
+        start_date = date(2019, 7, 23)
+        end_date = date(2019, 7, 27)
         for single_date in daterange(start_date, end_date):
             print(single_date.strftime("%Y-%m-%d"))
 
@@ -94,4 +96,67 @@ def find_all_pages(event, context):
 
 # find_all_pages(None, None)
 # download_pages(None, None)
-scrape_pages(None, None)
+# scrape_pages(None, None)
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--file', default="/tmp/pages.csv", help='Destination file for pages to be written')
+@click.option('--date-start', type = click.DateTime(formats=["%Y-%m-%d"]), default=str(date.today()), help='number of greetings')
+@click.option('--date-end', type = click.DateTime(formats=["%Y-%m-%d"]), default=str(date.today()), help='number of greetings')
+def find_all_pages(file, date_start, date_end):
+    date_start = date_start.date()
+    date_end = date_end.date()
+    click.echo(f"Writing pages from {date_start} to {date_end} into {file}")
+    with open(file, "a") as pages_file:
+        writer = csv.writer(pages_file, delimiter=",")
+        for single_date in daterange(date_start, date_end):
+            base_link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/plus/0?land_id_ab=&land_id_zu=&leihe=true&datum={single_date}"
+            link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/plus/1/page/{page}?land_id_ab=&land_id_zu=&leihe=true&datum={single_date}"
+            for page in scraper.find_all_pages(base_link_template.format(single_date=single_date)):
+                writer.writerow([link_template.format(page=page, single_date=single_date)])
+
+
+@click.command()
+@click.option('--file', default="/tmp/pages.csv", help='Source file for pages to be downloaded')
+def download_pages(file):
+    click.echo(f"Downloading pages from {file}")
+    with open(file, "r") as pages_file:
+        reader = csv.reader(pages_file, delimiter=",")
+
+        for row in reader:
+            url = row[0]
+            print(url)
+
+            parts = url.split("/")
+            single_date = parts[-5]
+            page = parts[-1]
+
+            page_path = "data/days/{single_date}-{page}.html".format(single_date=single_date, page=page)
+            if not os.path.isfile(page_path):
+                headers = {'user-agent': 'my-app/0.0.1'}
+                response = requests.get(url, stream=True, headers=headers)
+                with open(page_path, "wb") as handle:
+                    for data in response.iter_content():
+                        handle.write(data)
+
+@click.command()
+@click.option('--file', default="/tmp/transfers.json", help='Destination file for scraped results to be written')
+def scrape_pages(file):
+    with open(file, "w") as transfers_file:
+        for file_path in glob.glob("data/days/top*.html"):
+            print(file_path)
+            for row in scraper.scrape_transfers2(file_path):
+                print(row)
+                json.dump(row,transfers_file)
+                transfers_file.write("\n")
+
+cli.add_command(find_all_pages)
+cli.add_command(download_pages)
+cli.add_command(scrape_pages)
+
+if __name__ == '__main__':
+    cli()
