1de09264f6a58e9ca136ae07ca94e5a9fad4b091
Jan Gerling <jan.gerling@posteo.de>
2019-11-29 10:45:18 +0100

    refactored handler.py to include full pipeline to mine all transfers for a year range, working on a pipeline to download all transfers for a club

--- handler.py
+++ handler.py
@@ -1,21 +1,20 @@
 import csv
 from datetime import timedelta, date, datetime
-from dateutil import parser
-
 import requests
-
 import lib.scraper as scraper
-
 import glob
-
 import os.path
 import json
 import click
-from datetime import date
 
 
-def daterange(start_date, end_date):
+def date_range(start_date, end_date, ):
     for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+def date_range_year(start_date, end_date):
+    for n in range(int((end_date - start_date).years)):
         yield start_date + timedelta(n)
 
 
@@ -29,104 +28,33 @@ def move_file(from_path: str, to_path:str):
     os.rename(from_path, to_path)
 
 
-def scrape_transfers(event, context):
-    print("event: {event}".format(event=event))
-
-    with open("/tmp/transfers.json", "w") as transfers_file:
-        writer = csv.writer(transfers_file, delimiter=",")
-
-        writer.writerow(["season",
-            "playerUri", "playerName", "playerPosition", "playerAge",
-            "sellerClubUri", "sellerClubName", "sellerClubCountry",
-            "buyerClubUri", "buyerClubName", "buyerClubCountry",
-            "transferUri", "transferFee",
-            "playerImage", "playerNationality",
-            "timestamp"])
-
-        for file_path in glob.glob("data/days/*.html"):
-            print(file_path)
-            for row in scraper.scrape_transfers(file_path):
-                writer.writerow(list(row))
-
-def scrape_pages(event, context):
-    print("event: {event}".format(event=event))
-
-    with open("/tmp/transfers.json", "w") as transfers_file:
-        writer = csv.writer(transfers_file, delimiter=",")
-
-        # for file_path in glob.glob("data/days/*.html"):
-        for file_path in glob.glob("data/days/top*.html"):
-            print(file_path)
-            for row in scraper.scrape_transfers2(file_path):
-                print(row)
-                json.dump(row,transfers_file)
-                transfers_file.write("\n")
-
-
-def download_pages(event, context):
-    with open("/tmp/all_pages.csv", "r") as pages_file:
-        reader = csv.reader(pages_file, delimiter=",")
-
-        for row in reader:
-            url = row[0]
-            print(url)
-
-            parts = url.split("/")
-            single_date = parts[-5]
-            page = parts[-1]
-
-            page_path = "data/days/{single_date}-{page}.html".format(single_date=single_date, page=page)
-            if not os.path.isfile(page_path):
-                headers = {'user-agent': 'my-app/0.0.1'}
-                response = requests.get(url, stream=True, headers=headers)
-                with open(page_path, "wb") as handle:
-                    for data in response.iter_content():
-                        handle.write(data)
-
-
-def find_all_pages(event, context):
-    with open("/tmp/all_pages.csv", "a") as pages_file:
-        writer = csv.writer(pages_file, delimiter=",")
-
-        print("event: {event}".format(event=event))
-
-        start_date = date(2019, 7, 23)
-        end_date = date(2019, 7, 27)
-        for single_date in daterange(start_date, end_date):
-            print(single_date.strftime("%Y-%m-%d"))
-
-            base_link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/plus/0?land_id_ab=&land_id_zu=&leihe=true&datum={single_date}"
-            # base_link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/land_id_ab//land_id_zu/leihe/datum/{single_date}/plus/0/page/0"
-            link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/plus/1/page/{page}?land_id_ab=&land_id_zu=&leihe=true&datum={single_date}"
-            # link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/land_id_ab//land_id_zu//leihe/datum/{single_date}/plus/1/page/{page}?leihe=true"
-            for page in scraper.find_all_pages(base_link_template.format(single_date=single_date)):
-                writer.writerow([link_template.format(page=page, single_date=single_date)])
-
-@click.group()
-def cli():
-    pass
-
-
-@click.command()
-@click.option('--file', default="/tmp/all_pages.csv", help='Destination file for pages to be written')
-@click.option('--date-start', type = click.DateTime(formats=["%Y-%m-%d"]), default=str(date.today()), help='number of greetings')
-@click.option('--date-end', type = click.DateTime(formats=["%Y-%m-%d"]), default=str(date.today()), help='number of greetings')
-def find_all_pages(file, date_start, date_end):
+def __find_all_pages(file, date_start, date_end):
     date_start = date_start.date()
     date_end = date_end.date()
     click.echo(f"Writing pages from {date_start} to {date_end} into {file}")
-    with open(file, "a+", encoding="utf8") as pages_file:
+    with open(file, "w+", encoding="utf8") as pages_file:
         writer = csv.writer(pages_file, delimiter=",")
-        for single_date in daterange(date_start, date_end):
+        for single_date in date_range(date_start, date_end):
             base_link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/plus/0?land_id_ab=&land_id_zu=&leihe=true&datum={single_date}"
             link_template = "https://www.transfermarkt.co.uk/transfers/transfertagedetail/statistik/top/plus/1/page/{page}?land_id_ab=&land_id_zu=&leihe=true&datum={single_date}"
             for page in scraper.find_all_pages(base_link_template.format(single_date=single_date)):
                 writer.writerow([link_template.format(page=page, single_date=single_date)])
 
 
-@click.command()
-@click.option('--file', default="/tmp/all_pages.csv", help='Source file for pages to be downloaded')
-def download_pages(file):
+def __download_club_pages(clubs):
+    click.echo(f"Downloading pages for all {len(clubs)} clubs.")
+    for name, link in clubs.items():
+        link_template = "https://www.transfermarkt.co.uk"
+        url = link_template + link.replace("startseite", "alletransfers")
+        headers = {'user-agent': 'my-app/0.0.1'}
+        response = requests.get(url, stream=True, headers=headers)
+        club_path = "tmp/clubs/{club}.html".format(club=name)
+        with open(club_path, "wb+") as handle:
+            for data in response.iter_content():
+                handle.write(data)
+
+
+def __download_pages(file):
     click.echo(f"Downloading pages from {file}")
     with open(file, "r", encoding="utf8") as pages_file:
         reader = csv.reader(pages_file, delimiter=",")
@@ -135,7 +63,6 @@ def download_pages(file):
             # filter empty lines
             if len(row) > 0 and len(row[0]) > 10:
                 url = row[0]
-                print(url)
 
                 single_date = url.split("datum=")[-1]
                 page = (url.split("/")[-1]).split("?")[0]
@@ -149,24 +76,99 @@ def download_pages(file):
                             handle.write(data)
 
 
-
-@click.command()
-@click.option('--file', default="/tmp/transfers.json", help='Destination file for scraped results to be written')
-def scrape_pages(file):
+def __scrape_pages(file):
+    click.echo(f"Scraping all transfers from {file}")
     with open(file, "w+", encoding="utf8") as transfers_file:
         for file_path in glob.glob("tmp/days/*.html"):
             for row in scraper.scrape_transfers2(file_path):
                 json.dump(row, transfers_file)
                 transfers_file.write("\n")
-            click.echo(f"Scraped all transfers from {file_path}")
-
             dir_path, file_name = os.path.split(file_path)
             move_file(file_path, dir_path + "/processed/" + file_name)
+
+
+def __scrape_club_pages(file):
+    click.echo(f"Scraping all transfers from {file}")
+    with open(file, "w+", encoding="utf8") as clubs_file:
+        for file_path in glob.glob("tmp/clubs/*.html"):
+            for row in scraper.scrape_clubs(file_path):
+                json.dump(row, clubs_file)
+                clubs_file.write("\n")
+            dir_path, file_name = os.path.split(file_path)
+            move_file(file_path, dir_path + "/processed/" + file_name)
+
+
+def __extract_transfers_ind(year_start, year_end):
+    for year in range(year_start, year_end):
+        print("------------------", str(year), "------------------")
+        dir_path = os.path.dirname(__file__)
+        pages_file = dir_path + "/tmp/transfer_pages.csv"
+        date_start = datetime.strptime(str(year) + "-01-01", "%Y-%m-%d")
+        date_end = datetime.strptime(str(year + 1) + "-01-01", "%Y-%m-%d")
+
+        __find_all_pages(pages_file, date_start, date_end)
+        __download_pages(pages_file)
+
+        transfers_file = dir_path + "/data/transfers" + str(year) + ".json"
+        __scrape_pages(transfers_file)
+
+
+def __extract_transfers_club(year_start, year_end):
+    for year in range(year_start, year_end):
+        print("------------------------------------")
+        dir_path = os.path.dirname(__file__)
+        clubs = {'"El Ahly': '/el-ahly-kairo/startseite/verein/7',
+                'Atl\u00e9tico Madrid': '/atletico-madrid/startseite/verein/13'}
+
+        __download_club_pages(clubs)
+
+        transfers_file = dir_path + "/data/transfers_club" + str(year) + ".json"
+        __scrape_pages(transfers_file)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--file', default="/tmp/transfer_pages.csv", help='Destination file for pages to be written')
+@click.option('--date-start', type = click.DateTime(formats=["%Y-%m-%d"]), default=str(date.today()), help='number of greetings')
+@click.option('--date-end', type = click.DateTime(formats=["%Y-%m-%d"]), default=str(date.today()), help='number of greetings')
+def find_all_pages(file, date_start, date_end):
+    __find_all_pages(file, date_start, date_end)
+
+
+@click.command()
+@click.option('--file', default="/tmp/transfer_pages.csv", help='Source file for pages to be downloaded')
+def download_pages(file):
+    __download_pages(file)
+
+
+@click.command()
+@click.option('--file', default="/data/transfers.json", help='Destination file for scraped results to be written')
+def scrape_pages(file):
+    __scrape_pages(file)
+
+
+@click.command()
+@click.option('--year-start', type=str, default="2018", help='start year')
+@click.option('--year-end', type=str, default="2019", help='end year')
+def __extract_transfers_ind(year_start, year_end):
+    __extract_transfers_ind(int(year_start), int(year_end))
+
+
+@click.command()
+def __extract_transfers_club():
+    __extract_transfers_club()
 
 
 cli.add_command(find_all_pages)
 cli.add_command(download_pages)
 cli.add_command(scrape_pages)
+cli.add_command(__extract_transfers_ind)
+cli.add_command(__extract_transfers_club)
+
 
 if __name__ == '__main__':
     cli()
--- lib/scraper.py
+++ lib/scraper.py
@@ -6,12 +6,10 @@ import soupsieve as sv
 from pathvalidate import sanitize_filename
 
 def find_all_pages(page):
-    print(page)
     headers = {'user-agent': 'my-app/0.0.1'}
-    r = requests.get(page, headers = headers)
+    r = requests.get(page, headers= headers)
 
     page = BeautifulSoup(r.content, "html.parser")
-
     highest_page_link_element = page.select("li.letzte-seite a")
 
     if len(highest_page_link_element) > 0:
@@ -25,61 +23,9 @@ def find_all_pages(page):
         yield 0
 
 
-def scrape_transfers(page):
-    page = BeautifulSoup(open(page, "r"), "html.parser")
-
-    date = page.select("div.box h2")[0].text.replace("Transfer on ", "")
-    parsed_date = parser.parse(date)
-    timestamp = parsed_date.timestamp()
-
-    if parsed_date.month >= 7:
-        season = "{start}/{end}".format(start = parsed_date.year, end = parsed_date.year+1)
-    else:
-        season = "{start}/{end}".format(start = parsed_date.year-1, end = parsed_date.year)
-
-    for row in page.select("div#yw1 table.items tbody > tr"):
-        columns = row.select("> td")
-
-        if len(columns) == 7:
-            player_image_element = columns[0].select("img")[0]
-            player_element = columns[0].select("a.spielprofil_tooltip")[0]
-            player_age = columns[1].text
-            player_position = columns[0].select("table tr td")[-1].text
-
-            from_club_elements = columns[3].select("td.hauptlink a.vereinprofil_tooltip")
-            from_club_href = from_club_elements[0]["href"] if len(from_club_elements) > 0 else ""
-            from_club_text = from_club_elements[0].text if len(from_club_elements) > 0 else ""
-
-            from_club_country_image = columns[3].select("table tr td")[-1].select("img")
-            from_club_country = from_club_country_image[0]["title"] if len(from_club_country_image) > 0 else ""
-
-            to_club_elements = columns[4].select("td.hauptlink a.vereinprofil_tooltip")
-            to_club_href = to_club_elements[0]["href"] if len(to_club_elements) > 0 else ""
-            to_club_text = to_club_elements[0].text if len(to_club_elements) > 0 else ""
-
-            to_club_country_image = columns[4].select("table tr td")[-1].select("img")
-            to_club_country = to_club_country_image[0]["title"] if len(to_club_country_image) > 0 else ""
-
-            fee_element = columns[6].select("a")[0]
-
-            nationality = columns[2].select("img")[0]["title"]
-
-            yield (season,
-                   player_element["href"],
-                   player_element.text,
-                   player_position,
-                   player_age,
-                   from_club_href,
-                   from_club_text,
-                   from_club_country,
-                   to_club_href,
-                   to_club_text,
-                   to_club_country,
-                   fee_element["href"],
-                   fee_element.text,
-                   player_image_element["src"],
-                   nationality,
-                   int(timestamp))
+def scrape_clubs(page):
+    page = BeautifulSoup(open(page, "r", encoding="utf8"), "html.parser")
+    yield {"season": "UNK"}
 
 
 def scrape_transfers2(page):
